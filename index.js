4e118aad0c52fe48166730e4c7987663312228ae
leonbyrdjr <leonbyrdjr@Leons-MacBook-Pro.local>
2022-06-23 17:40:07 -0500

    commit tree 1e1e95dfc7c9a888feb6ad26c1e21a0b355dc94f

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code your solution here
+function superbowlWin(record){
+    // for(let result of record){
+    //     if(record.result === "W"){
+    //         return record.year
+    //     } else {
+
+    //     }
+    // }
+let result = record.find(record => record.result === "W")
+    return !!result ? result.year : undefined
+}
